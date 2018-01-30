e891194c43942796988ce9b137cec075bac153bc
Catle1337 <mdos432@gmail.com>
2018-01-30 23:52:39 +0300

    trading

--- Big_Capital/Big_Capital/Capital_Logic/stock_exchange.cs
+++ Big_Capital/Big_Capital/Capital_Logic/stock_exchange.cs
@@ -8,5 +8,41 @@ namespace Big_Capital.Capital_Logic
 {
     class stock_exchange
     {
+        order[] order_sell;
+        order[] order_buy;
+        currency[] quotations;
+        public stock_exchange(currency[] c)
+        {
+            this.quotations = c;
+        }
+        public void Show()
+        {
+            Console.Write("\n\tВалютные пары:\n\tНаименование:\t\t\tЦена:");
+            for (int i = 0; i < quotations.Length; i++)
+                Console.Write(i + " " + quotations[i].GetName() + "\t\t\t" + quotations[i].Cost);
+        }
+        public void Trade()
+        {
+            Int32 cur1;
+            Int32 cur2;
+            Show();
+            Console.Write("\tДля начала торгов введите номер 1 валюты и номер 2 валюты через пробел");
+            string[] tokens = Console.ReadLine().Split();
+            Int32 one = Convert.ToInt32(tokens[0]);
+            Int32 two = Convert.ToInt32(tokens[1]);
+            Console.Writeline();
+        }
+    }
+    class order
+    {
+        Double count;
+        Currency cur1;
+        Currency cur2;
+        public order(Double count, Currency cur1, Currency cur2)
+        {
+            this.cur1 = cur1;
+            this.count = count;
+            this.cur2 = cur2;
+        }
     }
 }
--- Big_Capital/Big_Capital/Program.cs
+++ Big_Capital/Big_Capital/Program.cs
@@ -10,7 +10,7 @@ namespace Big_Capital
     {
         static void Main(string[] args)
         {
-          
+            
         }
     }
 }
