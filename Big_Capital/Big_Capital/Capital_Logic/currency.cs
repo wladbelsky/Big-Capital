2cc4aa5e7a83aa741cb5b001773084240142eb7d
wladbelsky <raf520wb@gmail.com>
2018-01-30 22:21:03 +0300

    currency class

--- Big_Capital/Big_Capital/Capital_Logic/currency.cs
+++ Big_Capital/Big_Capital/Capital_Logic/currency.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Big_Capital.Capital_Logic
 {
-    class currency
+    class Currency
     {
+        protected string name;
+        Double cost;
+        public Currency()
+        {
+            name = "";
+            cost = 0;
+        }
+        public Currency(string n, Double cost = 0)
+        {
+            name = n;
+            this.cost = cost;
+        }
+
+        public Double Cost
+        {
+            get { return cost; }
+            set
+            {
+                if (value > 0)
+                    cost = value;
+                else
+                    cost = 0;
+            }
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
     }
 }
---
+++ Big_Capital/Big_Capital/Capital_Logic/playerInterface.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Big_Capital.Capital_Logic
+{
+    class PlayerInterface
+    {
+        string name;
+        CurOwned[] wallet;
+
+        public PlayerInterface(string name)
+        {
+            this.name = name;
+        }
+        public PlayerInterface(string name, CurOwned[] own)
+        {
+            this.name = name;
+            wallet = own;
+        }
+        public PlayerInterface(string name, Currency[] cur)
+        {
+            this.name = name;
+            wallet = new CurOwned[cur.Length];
+            for(int i = 0; i < cur.Length; i++)
+            {
+                wallet[i] = new CurOwned(cur[i], 0);
+            }
+        }
+    }
+
+    class CurOwned : Currency   //Валюта пользователя
+    {
+        Double count;
+
+        public CurOwned(Currency cur, Double own)
+        {
+            name = cur.GetName();
+            Cost = cur.Cost;
+        }
+        public CurOwned(string n, Double own , Double cost = 0) : base(n, cost)
+        {
+            count = own;
+        }
+        public Double Owned
+        {
+            get { return count; }
+            set
+            {
+                if (value > 0)
+                    count = value;
+                else
+                    count = 0;
+            }
+        }
+    }
+}
